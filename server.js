e8989b61f166d07e4a6c03c701fb65ffb7e8e63c
davidmichaelbrown <davidmichaelbrown1@gmail.com>
2013-11-19 19:51:24 -0500

    basic server

--- server.js
+++ server.js
@@ -7,60 +7,59 @@ var socketio = require('socket.io');
 
 
 // *http helper functions*
-// 404
 function send404(response) {
-	response.writeHead(404, {'content-type': 'text/plain'});
-	response.write('Error 404: resource not found');
-	response.end()
+    response.writeHead(404, {'content-type': 'text/plain'});
+    response.write('Error 404: resource not found');
+    response.end()
 }
 
 
 // serve file
 function sendFile(response, filePath, fileContents) {
-	response.writeHead(
-		200,
-		{'content-type': mime.lookup(path.basename(filePath))}
-	);
-	response.end(fileContents);
+    response.writeHead(
+        200,
+        {'content-type': mime.lookup(path.basename(filePath))}
+    );
+    response.end(fileContents);
 }
 
 
 // serve static file
 function serveStatic(response, cache, absPath) {
-	if (cache[absPath]) {
-		sendFile(response, absPath, cache[absPath]);
-	} else {
-		fs.exists(absPath, function(exists) {
-			if (exists) {
-				fs.readFile(absPath, function(err, data) {
-					if (err) {
-						send404(response);
-					} else {
-						cache[absPath] = data;
-						sendFile(response, absPath, data);
-					}
-				});
-			} else {
-				send404(response);
-			}
-		});
-	}
+    if (cache[absPath]) {
+        sendFile(response, absPath, cache[absPath]);
+    } else {
+        fs.exists(absPath, function(exists) {
+            if (exists) {
+                fs.readFile(absPath, function(err, data) {
+                    if (err) {
+                        send404(response);
+                    } else {
+                        cache[absPath] = data;
+                        sendFile(response, absPath, data);
+                    }
+                });
+            } else {
+                send404(response);
+            }
+        });
+    }
 };
 
 
 // server
 var server = http.createServer(function(request, response) {
-	var filePath = false;
+    var filePath = false;
 
-	if (request.url == '/') {
-		filePath = 'public/index.html';
-	} else {
-		filePath = 'public' + request.url;
-	}
-	var absPath = './' + filePath;
-	serveStatic(response, cache, absPath);
+    if (request.url == '/') {
+        filePath = 'public/index.html';
+    } else {
+        filePath = 'public' + request.url;
+    }
+    var absPath = './' + filePath;
+    serveStatic(response, cache, absPath);
 });
 
 server.listen(3000, function() {
-	console.log('server is running at http://localhost:3000/')
+    console.log('server is running at http://localhost:3000/')
 });
