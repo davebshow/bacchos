5d9599c86a4df1b8651ddb42f97e6d873aaedd20
davidmichaelbrown <davidmichaelbrown1@gmail.com>
2013-11-22 17:10:08 -0500

    fixed typo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express()
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.logger('log'));
+app.use(express.logger('dev'));
 app.use(express.static(__dirname + '/public'))
 app.use(app.router)
 
