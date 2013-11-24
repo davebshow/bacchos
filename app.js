9026554ac414ce1d0b9d352b9fe2ec8e4540a487
davidmichaelbrown <davidmichaelbrown1@gmail.com>
2013-11-23 22:02:16 -0500

    working on query form post, may change to get but post for now

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 var express = require('express');
-var app = express()
+var app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.logger('dev'));
-app.use(express.static(__dirname + '/public'))
-app.use(app.router)
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.static(__dirname + '/public'));
+app.use(app.router);
 
-app.get('/', function(request, response){
-	response.render('index', {title : 'Home'});
+app.get('/', function(req, res){
+	res.render('index', {title : 'Home'});
 });
 
-app.post('/ajax/map', function(request, response) {
+app.post('/ajax/map', function(req, res) {
+	//console.log(req.params[0]);
+	console.log(req.body);
+	//console.log(req.route);
+	//console.log(req.cookie);
 	data = '[{"lat": 41, "lng": -81}, {"lat":44, "lng": -122}]'
-	response.end(data);
+	res.end(data);
 	console.log('SENT DATA');
 });
 
