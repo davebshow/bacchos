5cf1911a43c56b517a8258700d09563dc7c9f5fd
davidmichaelbrown <davidmichaelbrown1@gmail.com>
2013-11-24 01:46:04 -0500

    added forbidden for non-ajax requests

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
@@ -19,7 +21,8 @@ app.post('/ajax/map', function(req, res) {
 	if (is_ajax_request) {
 		var data = '[{"lat": 41, "lng": -81}, {"lat":44, "lng": -122}]'
 		res.end(data);
-		console.log('SENT DATA');
+	} else {
+		res.end(403, 'forbidden')
 	}
 });
 
