f213a834f31d6be9e158b241d7f208a8ecf056b0
davidmichaelbrown <davidmichaelbrown1@gmail.com>
2014-01-19 15:15:59 -0500

    using leaflet directive library

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,24 +1,17 @@
-angular.module('bacchosApp', [])
-
-	
-
-	.directive('map', function ($parse) {
-
-	var directiveDefObj = {
-		restrict: 'E',
-		replace: false,
-		link: function(scope, element, attrs) {
-			// instantiate map
-    		var map = L.map(element[0]).setView([44, -82], 8);
-    		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    		}).addTo(map);
-    	}
-			
-	};
-	return directiveDefObj;
-
-})
+var app = angular.module('bacchosApp', ['leaflet-directive']);
 
 
-//bacchosApp.
+app.controller("MapCtrl", ['$scope', function ($scope) {
+    angular.extend($scope, {
+		center: {
+            lat: 40.095,
+            lng: -3.823,
+            zoom: 6
+        },
+        markers: {},
+        defaults: {
+            scrollWheelZoom: false
+        }
+    });
+}]);
+
